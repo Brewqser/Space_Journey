1628113dde7b594649c6a22b14b71d501f725db6
Brewqser <michal.czwornog124812@gmail.com>
2019-09-07 16:43:53 +0200

    Update player movment.

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame import Vector2
+from math import sin, cos, pi
 
 
 class Player(object):
@@ -9,11 +10,12 @@ class Player(object):
 
         win_size = self.game.screen.get_size()
         self.pos = Vector2(win_size[0] / 2, win_size[1] / 2)
-        self.vel = Vector2(0, -0.01)  # Starting vel set to - 0.01 for right beginning angle
+        self.vel = Vector2(0, 0)
         self.acc = Vector2(0, 0)
+        self.angle = 180
 
         print(self.pos)
-        self.speed = 1
+        self.speed = 0.55
         self.drag = 0.98
 
     def add_force(self, force):
@@ -22,15 +24,20 @@ class Player(object):
     def tick(self):
 
         # Input
+        rot = 5
+        rad_ang = self.angle * pi / 180
+
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_w]:
-            self.add_force(Vector2(0, -self.speed))
+            self.add_force(Vector2(self.speed * sin(rad_ang), self.speed * cos(rad_ang)))
         if pressed[pygame.K_s]:
-            self.add_force(Vector2(0, self.speed))
+            self.add_force(Vector2(-0.35 * self.speed * sin(rad_ang), -0.35 * self.speed * cos(rad_ang)))
         if pressed[pygame.K_a]:
-            self.add_force(Vector2(-self.speed, 0))
+            self.angle += rot
         if pressed[pygame.K_d]:
-            self.add_force(Vector2(self.speed, 0))
+            self.angle -= rot
+
+        self.angle %= 360
 
         # Physic
         self.vel *= self.drag
@@ -43,15 +50,12 @@ class Player(object):
         self.pos.x %= win_size[0]
         self.pos.y %= win_size[1]
 
-        # print(self.vel)
-
     def draw(self):
         # Player
         points = [Vector2(0, -10), Vector2(5, 5), Vector2(0, 2), Vector2(-5, 5)]
 
         # Rotation
-        angle = self.vel.angle_to(Vector2(0, 1))
-        points = [p.rotate(angle) for p in points]
+        points = [p.rotate(self.angle) for p in points]
         points = [Vector2(p.x, p.y * -1) for p in points]
 
         # Update position
